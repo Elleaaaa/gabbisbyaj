d042ee4c09792433e0c277cbb2afb2e12a53a980
sherwin <sherwin.gomez@mbfn.ph>
2026-02-23 22:31:39 +0800

    resolve conflicts during merge

--- lib/waypoints/links.php
+++ lib/waypoints/links.php
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 <?php
   $links = array(
     'js' => 'lib/waypoints/waypoints.min.js'
   );
 ?>
-=======
-<?php
-  $links = array(
-    'js' => 'lib/waypoints/waypoints.min.js'
-  );
-?>
->>>>>>> 743d63db6b5c2a2e597ecfd231fe1ba701129d69
